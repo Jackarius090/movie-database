77ed9ab7e1d85659a2e410ba8af86207532ddb58
Jack Dickinson <jackdickinson090@hotmail.com>
2026-07-03 19:51:46 +0200

    start of movie card

---
+++ app/components/MovieCard.tsx
@@ -0,0 +1,13 @@
+export default function MovieCard() {
+  return (
+    <section className="flex border rounded-md p-4 gap-16">
+      <div>Movie name</div>
+      <div>Genre</div>
+      <div>Rating</div>
+      <div>Year</div>
+      <div>Director</div>
+      <div>Stars</div>
+      <div>Length</div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import MovieCard from "./components/MovieCard";
+
 export default function Home() {
   return (
-    <main className="flex justify-center">
+    <main className="flex flex-col justify-center items-center">
       <header>
         <h1 className="text-5xl p-8">Movies database</h1>
       </header>
+      <section>
+        <MovieCard />
+      </section>
     </main>
   );
 }
